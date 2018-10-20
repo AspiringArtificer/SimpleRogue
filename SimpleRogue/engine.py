78d7347f90451af33b3eb1efc2a6bebbf1711a27
blu12758 <willbill8202@yahoo.com>
2018-10-20 00:57:04 -0500

    implemented global render functions

--- SimpleRogue/engine.py
+++ SimpleRogue/engine.py
@@ -4,14 +4,15 @@ import libtcodpy as libtcod
 #Project Imports
 from entity import Entity
 from input_handlers import handle_keys
+from render_functions import clear_all, render_all
 
 def main():
     #initilize main variables
     screen_width = 80
     screen_height = 50
 
-    player = Entity(int(screen_width/2), int(screen_height)/2, '@', libtcod.white)
-    npc = Entity(int(screen_width/2 -5), int(screen_height/2), '@', libtcod.yellow)
+    player = Entity(int(screen_width/2), int(screen_height/2), '@', libtcod.white)
+    npc = Entity(int(screen_width/2 - 5), int(screen_height/2), '@', libtcod.yellow)
     entities = [npc, player]
 
     #set up screen
@@ -27,12 +28,9 @@ def main():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
         
         #Update Display
-        libtcod.console_set_default_foreground(con, libtcod.white)
-        libtcod.console_put_char(con,player_x, player_y, '@', libtcod.BKGND_NONE)
-        libtcod.console_blit(con, 0, 0,screen_width, screen_height, 0, 0, 0)
+        render_all(con, entities, screen_width, screen_height)
         libtcod.console_flush()
-
-        libtcod.console_put_char(con, player_x, player_y, ' ', libtcod.BKGND_NONE)
+        clear_all(con, entities)
 
         action = handle_keys(key)
 
@@ -41,6 +39,7 @@ def main():
         fullscreen = action.get('fullscreen')
 
         if move:
+            dx, dy = move
             player.move(dx, dy)
 
         if exit:
