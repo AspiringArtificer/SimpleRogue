bf914a376f32829fe7d92ca37bc881ed097bae2a
blu12758 <willbill8202@yahoo.com>
2018-10-20 19:47:06 -0500

    added basic rendering, randomized map, and field of view

--- SimpleRogue/engine.py
+++ SimpleRogue/engine.py
@@ -4,12 +4,31 @@ import libtcodpy as libtcod
 #Project Imports
 from entity import Entity
 from input_handlers import handle_keys
+from map_objects.game_map import GameMap
 from render_functions import clear_all, render_all
+from fov_functions import initializ_fov, recompute_fov
 
 def main():
     #initilize main variables
     screen_width = 80
     screen_height = 50
+    map_width = 80
+    map_height = 45
+
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
+
+    fov_algorithm = 0
+    fov_light_walls = True
+    fov_radius = 10
+
+    colors = {
+        'dark_wall': libtcod.Color(55, 50, 45),
+        'dark_ground': libtcod.Color(15, 15, 15),
+        'light_wall': libtcod.Color(70, 55, 40),
+        'light_ground': libtcod.Color(30, 20, 15)
+    }
 
     player = Entity(int(screen_width/2), int(screen_height/2), '@', libtcod.white)
     npc = Entity(int(screen_width/2 - 5), int(screen_height/2), '@', libtcod.yellow)
@@ -18,17 +37,26 @@ def main():
     #set up screen
     libtcod.console_set_custom_font('arial12x12.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
     libtcod.console_init_root(screen_width, screen_height, 'SimpleRogue', False)
-
     con = libtcod.console_new(screen_width, screen_height)
     key = libtcod.Key()
     mouse = libtcod.Mouse()
-    
+
+    game_map = GameMap(map_width, map_height)
+    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player)
+
+    fov_recompute = True
+    fov_map = initializ_fov(game_map)
+
     # main game loop
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
         
         #Update Display
-        render_all(con, entities, screen_width, screen_height)
+        if fov_recompute:
+            recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
+
+        render_all(con, entities, game_map, fov_map, fov_recompute, screen_width, screen_height, colors)
+        fov_recompute = False
         libtcod.console_flush()
         clear_all(con, entities)
 
@@ -40,7 +68,9 @@ def main():
 
         if move:
             dx, dy = move
-            player.move(dx, dy)
+            if not game_map.is_blocked(player.x + dx, player.y + dy):
+                player.move(dx, dy)
+                fov_recompute = True
 
         if exit:
             return True
---
+++ SimpleRogue/map_objects/tile.py
@@ -0,0 +1,16 @@
+class Tile:
+    """
+    A single location on a map. It may be blocking, open, transparent, etc.
+    """
+
+    def __init__(self, blocked, block_sight=None):
+        self.blocked = blocked
+
+        #By default, if a tile is blocked, it also blocks sight
+        if block_sight is None:
+            block_sight = blocked
+
+        self.block_sight = block_sight
+
+        self.explored = False
+
